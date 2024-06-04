e94b8826987e354d2fc2a2db41f96fb3355fcda1
kimJiHwan0618 <78426129+kimJiHwan0618@users.noreply.github.com>
2024-06-04 23:39:22 +0900

    섹션9 포트폴리오 리뷰 이전 강의 수강 완료

---
+++ next_play-ground/pages/section06/06-01-container-pressenter/index.js
@@ -0,0 +1,6 @@
+import BoardWrite from '../../../src/components/units/board/write/BoardWrite.container';
+
+export default function GraphqlMutationPage() {
+  // 한줄일때는 괄호 필요 없음
+  return <BoardWrite />;
+}
