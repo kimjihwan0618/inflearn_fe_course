0890838a8ca9548cd4601160dd435385d8bb3cd7
kimJiHwan0618 <78426129+kimJiHwan0618@users.noreply.github.com>
2024-06-06 23:38:20 +0900

    section 11. 수강 자료 추가

---
+++ next_play-ground/src/components/units/board/07-write/BoardWrite.container.js
@@ -0,0 +1,62 @@
+import { useMutation } from '@apollo/client';
+import { useState } from 'react';
+import BoardWriteUI from './BoardWrite.presenter';
+import { 나의그래프큐엘셋팅 } from './BoardWrite.queries';
+
+export default function BoardWrite() {
+  const [isActive, setIsActive] = useState(false);
+  const [writer, setWriter] = useState();
+  const [title, setTitle] = useState();
+  const [contents, setContents] = useState();
+
+  const [나의함수] = useMutation(나의그래프큐엘셋팅);
+
+  const onClickSubmit = async () => {
+    const result = await 나의함수({
+      variables: {
+        // variabels 이게 $ 역할을 함
+        writer,
+        title,
+        contents,
+      },
+    });
+    console.log(result);
+  };
+
+  const onChangeWriter = (event) => {
+    setWriter(event.target.value);
+    if (event.target.value && title && contents) {
+      setIsActive(true);
+    } else {
+      setIsActive(false);
+    }
+  };
+
+  const onChangeTitle = (event) => {
+    setTitle(event.target.value);
+    if (writer && event.target.value && contents) {
+      setIsActive(true);
+    } else {
+      setIsActive(false);
+    }
+  };
+
+  const onChangeContents = (event) => {
+    setContents(event.target.value);
+    if (writer && title && event.target.value) {
+      setIsActive(true);
+    } else {
+      setIsActive(false);
+    }
+  };
+
+  return (
+    <BoardWriteUI
+      onClickSubmit={onClickSubmit}
+      onChangeWriter={onChangeWriter}
+      onChangeTitle={onChangeTitle}
+      onChangeContents={onChangeContents}
+      isActive={isActive}
+    />
+  );
+}
