5f9b79ab9870fadde9ab40934c642fec9bc64f04
kimJiHwan0618 <78426129+kimJiHwan0618@users.noreply.github.com>
2024-08-06 17:53:59 +0900

    sectino18 페이지네이션 수강중

---
+++ next_play-ground/pages/section15/15-01-pagination/index.tsx
@@ -0,0 +1,55 @@
+import { gql, useMutation, useQuery } from "@apollo/client";
+import type {
+  IQuery,
+  IQueryFetchBoardsArgs,
+} from "../../../src/commons/types/generated/types";
+
+const FETCH_BOARDS = gql`
+  query fetchBoards($page: Int) {
+    fetchBoards(page: $page) {
+      _id
+      writer
+      title
+      contents
+    }
+  }
+`;
+
+const DELETE_BOARD = gql`
+  mutation deleteBoard($number: Int) {
+    deleteBoard(number: $number) {
+      message
+    }
+  }
+`;
+
+export default function StaticRoutingMovedPage(): JSX.Element {
+  const { data, refetch } = useQuery<
+    Pick<IQuery, "fetchBoards">,
+    IQueryFetchBoardsArgs
+  >(FETCH_BOARDS);
+
+  const [deleteBoard] = useMutation(DELETE_BOARD);
+
+  console.log(data?.fetchBoards);
+
+  const onClickPage = (page: number): void => {
+    void refetch({ page });
+  };
+
+  return (
+    <div>
+      {data?.fetchBoards.map((el) => (
+        <div key={el._id}>
+          <span style={{ margin: "10px" }}>{el.title}</span>
+          <span style={{ margin: "10px" }}>{el.writer}</span>
+        </div>
+      ))}
+      {new Array(10).fill("").map((_, index) => (
+        <span key={index + 1} onClick={() => onClickPage(index + 1)}>
+          {index + 1}
+        </span>
+      ))}
+    </div>
+  );
+}
---
+++ next_play-ground/pages/section15/15-02-pagination-next/index.tsx
@@ -0,0 +1,72 @@
+import { gql, useMutation, useQuery } from "@apollo/client";
+import type {
+  IQuery,
+  IQueryFetchBoardsArgs,
+} from "../../../src/commons/types/generated/types";
+import { useState } from "react";
+
+const FETCH_BOARDS = gql`
+  query fetchBoards($page: Int) {
+    fetchBoards(page: $page) {
+      _id
+      writer
+      title
+      contents
+    }
+  }
+`;
+
+const DELETE_BOARD = gql`
+  mutation deleteBoard($number: Int) {
+    deleteBoard(number: $number) {
+      message
+    }
+  }
+`;
+
+export default function StaticRoutingMovedPage(): JSX.Element {
+  const [startPage, setStartPage] = useState(1);
+  const { data, refetch } = useQuery<
+    Pick<IQuery, "fetchBoards">,
+    IQueryFetchBoardsArgs
+  >(FETCH_BOARDS);
+
+  const [deleteBoard] = useMutation(DELETE_BOARD);
+
+  console.log(data?.fetchBoards);
+
+  const onClickPage = (page: number): void => {
+    void refetch({ page });
+  };
+
+  const onClickPrevPage = (): void => {
+    setStartPage((prev) => prev - 10);
+    void refetch({ page: startPage - 10 });
+  };
+
+  const onClickNextPage = (): void => {
+    setStartPage((prev) => prev + 10);
+    void refetch({ page: startPage + 10 });
+  };
+
+  return (
+    <div>
+      {data?.fetchBoards.map((el) => (
+        <div key={el._id}>
+          <span style={{ margin: "10px" }}>{el.title}</span>
+          <span style={{ margin: "10px" }}>{el.writer}</span>
+        </div>
+      ))}
+      <span onClick={onClickPrevPage}>이전페이지</span>
+      {new Array(10).fill("").map((_, index) => (
+        <span
+          key={index + startPage}
+          onClick={() => onClickPage(index + startPage)}
+        >
+          {index + startPage}
+        </span>
+      ))}
+      <span onClick={onClickNextPage}>다음페이지</span>
+    </div>
+  );
+}
