550efff1360433b846b23b1cf7f424408336a3fa
kimJiHwan0618 <78426129+kimJiHwan0618@users.noreply.github.com>
2024-08-07 11:20:24 +0900

    section18 수강 완료

--- next_play-ground/pages/section15/15-03-pagination-last/index.tsx
+++ next_play-ground/pages/section15/15-03-pagination-last/index.tsx
@@ -2,7 +2,6 @@ import { gql, useMutation, useQuery } from "@apollo/client";
 import type {
   IQuery,
   IQueryFetchBoardsArgs,
-  IQueryFetchBoardsCountArgs,
 } from "../../../src/commons/types/generated/types";
 import { useState } from "react";
 
@@ -25,42 +24,29 @@ const DELETE_BOARD = gql`
   }
 `;
 
-const FETCH_BOARDS_COUNT = gql`
-  query {
-    fetchBoardsCount
-  }
-`;
-
 export default function StaticRoutingMovedPage(): JSX.Element {
   const [startPage, setStartPage] = useState(1);
   const { data, refetch } = useQuery<
     Pick<IQuery, "fetchBoards">,
     IQueryFetchBoardsArgs
   >(FETCH_BOARDS);
-  const { data: dataBoardsCount } = useQuery<
-    Pick<IQuery, "fetchBoardsCount">,
-    IQueryFetchBoardsCountArgs
-  >(FETCH_BOARDS_COUNT);
-  const lastPage = Math.ceil(dataBoardsCount?.fetchBoardsCount ?? 10 / 10);
+
+  const [deleteBoard] = useMutation(DELETE_BOARD);
 
   console.log(data?.fetchBoards);
-  console.log(dataBoardsCount);
 
   const onClickPage = (page: number): void => {
     void refetch({ page });
   };
 
   const onClickPrevPage = (): void => {
-    if (startPage === 1) return;
     setStartPage((prev) => prev - 10);
     void refetch({ page: startPage - 10 });
   };
 
   const onClickNextPage = (): void => {
-    if (startPage + 10 <= lastPage) {
-      setStartPage((prev) => prev + 10);
-      void refetch({ page: startPage + 10 });
-    }
+    setStartPage((prev) => prev + 10);
+    void refetch({ page: startPage + 10 });
   };
 
   return (
@@ -72,18 +58,14 @@ export default function StaticRoutingMovedPage(): JSX.Element {
         </div>
       ))}
       <span onClick={onClickPrevPage}>이전페이지</span>
-      {new Array(10).fill("").map(
-        (_, index) =>
-          index + startPage <= lastPage && (
-            <span
-              key={index + startPage}
-              onClick={() => onClickPage(index + startPage)}
-              style={{ margin: "5px" }}
-            >
-              {index + startPage}
-            </span>
-          )
-      )}
+      {new Array(10).fill("").map((_, index) => (
+        <span
+          key={index + startPage}
+          onClick={() => onClickPage(index + startPage)}
+        >
+          {index + startPage}
+        </span>
+      ))}
       <span onClick={onClickNextPage}>다음페이지</span>
     </div>
   );
