d84d72c866f4b48328d96451154575e44088544e
kimJiHwan0618 <78426129+kimJiHwan0618@users.noreply.github.com>
2024-09-18 21:52:49 +0900

    useMemo useCallback 개념 학습

---
+++ next_play-ground/pages/section31/31-01-memoization/index.tsx
@@ -0,0 +1,40 @@
+import { useCallback, useMemo, useState } from 'react';
+
+export default function MemoizationPage(): JSX.Element {
+  console.log('컴포넌트가 렌더링 되었습니다.');
+
+  let countLet = 0;
+  const [countState, setCountState] = useState(0);
+
+  //  1. useMemo 변수 기억
+  const aaa = useMemo(() => Math.random(), []);
+  console.log(aaa);
+
+  // 2. useCallback으로 함수 기억
+  const onClickCountLet = useCallback((): void => {
+    console.log(countLet + 1);
+    countLet += 1;
+  }, []);
+
+  // 3. useCallback 사용시 주의사항 => state 사용 주의
+  const onClickCountState = useCallback((): void => {
+    // console.log(countState + 1);
+    setCountState((prev) => prev + 1);
+  }, []);
+
+  // 4. useMemo로 나만의 useCallback 만들어 보기
+  // const onClickCountState2 = useMemo(() => {
+  //   return (): void => {
+  //     setCountState(countState + 1);
+  //   };
+  // }, []);
+
+  return (
+    <>
+      <div>카운트(let) : {countLet}</div>
+      <button onClick={onClickCountLet}>카운트(let) +1 올리기</button>
+      <div>카운트(state) : {countState}</div>
+      <button onClick={onClickCountState}>카운트(state) +1 올리기</button>
+    </>
+  );
+}
