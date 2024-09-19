fcaa235be549b59666ec34652b5cb0c364cc9f47
kimJiHwan0618 <78426129+kimJiHwan0618@users.noreply.github.com>
2024-09-19 18:26:00 +0900

    Promise all을 사용한 다중 이미지 업로드 개선

---
+++ next_play-ground/pages/section31/31-07-image-upload-submit/index.tsx
@@ -0,0 +1,81 @@
+import { gql, useMutation } from '@apollo/client';
+import { useState, type ChangeEvent } from 'react';
+import type {
+  IMutation,
+  IMutationUploadFileArgs,
+} from '../../../src/commons/types/generated/types';
+
+const 나의그래프큐엘셋팅 = gql`
+  mutation {
+    createBoard(writer: "철수", title: "안녕하세요", contents: "반갑습니다") {
+      _id
+      number
+      message
+    }
+  }
+`;
+
+const UPLOAD_FILE = gql`
+  mutation uploadFile($file: Upload!) {
+    uploadFile(file: $file) {
+      url
+    }
+  }
+`;
+
+export default function ImageUploadPage(): JSX.Element {
+  const [나의함수] = useMutation(나의그래프큐엘셋팅);
+  const [file, setFile] = useState<File>();
+  const [uploadFile] = useMutation<Pick<IMutation, 'uploadFile'>, IMutationUploadFileArgs>(
+    UPLOAD_FILE
+  );
+  const [imageUrl, setImageUrl] = useState('');
+
+  const onChangeFile = async (event: ChangeEvent<HTMLInputElement>): Promise<void> => {
+    const file = event.target.files?.[0]; // 배열로 들어오는 이유 : <input type="file" multiple /> 일떄 여러개 드래그 가능
+    if (file === undefined) return;
+    console.log(file);
+    console.log('파일까지 나옴');
+    // const result = await uploadFile({
+    //   variables: {
+    //     file,
+    //   },
+    // });
+    // console.log(result.data?.uploadFile.url);
+    // setImageUrl(result.data?.uploadFile.url ?? '');
+
+    // 1. 임시URL 생성 => (가짜 URL - 내 브라우저에서만 접근 가능)
+    // const result = URL.createObjectURL(file);
+    // console.log(result);
+    // 2. 임시URL 생성 => (진짜 URL - 다른 브라우저에서도 접근 가능)
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(file);
+    fileReader.onload = (event) => {
+      console.log(event.target?.result); // 게시판에서 event.target.id를 쓰면 eslint가 잡았던 이유: event.target
+      if (typeof event.target?.result === 'string') {
+        setImageUrl(event.target?.result);
+        setFile(file);
+      }
+    };
+  };
+
+  const onClickSubmit = async () => {
+    // 1. uploadFile
+    const resultFile = await uploadFile({ variables: { file } });
+    const url = resultFile.data?.uploadFile.url;
+
+    // 2. createBoard
+    const result = await 나의함수();
+    console.log(result);
+  };
+
+  return (
+    <>
+      <input type="file" onChange={onChangeFile} />
+      <img src={imageUrl} />
+      {/* <img src={`https://storage.googleapis.com/${imageUrl}`} /> */}
+
+      <button onClick={onClickSubmit}>게시글 등록하기</button>
+    </>
+  );
+}
---
+++ next_play-ground/pages/section31/31-08-image-upload-submit-promise-all/index.tsx
@@ -0,0 +1,113 @@
+import { gql, useMutation } from '@apollo/client';
+import { useState, type ChangeEvent } from 'react';
+import type {
+  IMutation,
+  IMutationUploadFileArgs,
+} from '../../../src/commons/types/generated/types';
+
+const 나의그래프큐엘셋팅 = gql`
+  mutation {
+    createBoard(writer: "철수", title: "안녕하세요", contents: "반갑습니다") {
+      _id
+      number
+      message
+    }
+  }
+`;
+
+const UPLOAD_FILE = gql`
+  mutation uploadFile($file: Upload!) {
+    uploadFile(file: $file) {
+      url
+    }
+  }
+`;
+
+export default function ImageUploadPage(): JSX.Element {
+  const [나의함수] = useMutation(나의그래프큐엘셋팅);
+  const [files, setFiles] = useState<File[]>([]);
+  const [uploadFile] = useMutation<Pick<IMutation, 'uploadFile'>, IMutationUploadFileArgs>(
+    UPLOAD_FILE
+  );
+  const [imageUrls, setImageUrls] = useState(['', '', '']);
+
+  const onChangeFile =
+    (index: number) =>
+    async (event: ChangeEvent<HTMLInputElement>): Promise<void> => {
+      const file = event.target.files?.[0]; // 배열로 들어오는 이유 : <input type="file" multiple /> 일떄 여러개 드래그 가능
+      if (file === undefined) return;
+      console.log(file);
+      console.log('파일까지 나옴');
+      // const result = await uploadFile({
+      //   variables: {
+      //     file,
+      //   },
+      // });
+      // console.log(result.data?.uploadFile.url);
+      // setImageUrl(result.data?.uploadFile.url ?? '');
+
+      // 1. 임시URL 생성 => (가짜 URL - 내 브라우저에서만 접근 가능)
+      // const result = URL.createObjectURL(file);
+      // console.log(result);
+      // 2. 임시URL 생성 => (진짜 URL - 다른 브라우저에서도 접근 가능)
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(file);
+      fileReader.onload = (event) => {
+        console.log(event.target?.result); // 게시판에서 event.target.id를 쓰면 eslint가 잡았던 이유: event.target
+        if (typeof event.target?.result === 'string') {
+          const tempUrls = [...imageUrls];
+          tempUrls[index] = event.target?.result;
+          setImageUrls(tempUrls);
+
+          const tempFiles = [...files];
+          tempFiles[index] = file;
+          setFiles(tempFiles);
+        }
+      };
+    };
+
+  const onClickSubmit = async () => {
+    // 1. uploadFile
+    // 1-1) 안좋은예제 - await를 매번 기다림 => for문 사용해도 마찬가지 (이유 : i 값에 의존하기 때문에 ...)
+    // const resultFile0 = await uploadFile({ variables: { file: files[0] } });
+    // const resultFile1 = await uploadFile({ variables: { file: files[1] } });
+    // const resultFile2 = await uploadFile({ variables: { file: files[2] } });
+    // const url0 = resultFile0.data?.uploadFile.url;
+    // const url1 = resultFile1.data?.uploadFile.url;
+    // const url2 = resultFile2.data?.uploadFile.url;
+    // const resultUrls = [url0, url1, url2]
+
+    // 1-2) 좋은예제 - Promise.all 사용
+    // const results = await Promise.all([
+    //   uploadFile({ variables: { file: files[0] } }),
+    //   uploadFile({ variables: { file: files[1] } }),
+    //   uploadFile({ variables: { file: files[2] } }),
+    // ]);
+    // console.log(results); // [resultsFile0, resultFile1, resultFile2]
+    // const resultUrls = results.map((el) => el.data?.uploadFile.url);
+
+    // 1-3) 좋은예제 - Promise.all 사용 => 리팩토링
+    // const files = []
+    // files.map((el) => uploadFile({ variables: { file: el } }));
+
+    Promise.all(files.map((el) => uploadFile({ variables: { file: el } })));
+
+    // 2. createBoard
+    const result = await 나의함수();
+    console.log(result);
+  };
+
+  return (
+    <>
+      <input type="file" onChange={onChangeFile(0)} />
+      <input type="file" onChange={onChangeFile(1)} />
+      <input type="file" onChange={onChangeFile(2)} />
+      <img src={imageUrls[0]} />
+      <img src={imageUrls[1]} />
+      <img src={imageUrls[2]} />
+      {/* <img src={`https://storage.googleapis.com/${imageUrl}`} /> */}
+
+      <button onClick={onClickSubmit}>게시글 등록하기</button>
+    </>
+  );
+}
